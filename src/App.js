e7d03bdf3ef1be499042c0000357b61a924fb1ee
Liiukenan <305290526@qq.com>
2021-04-14 19:29:54 +0800

    悦玺清风官网

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import React from 'react'
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from './view/Home'
 import About from './view/About'
 import Product from './view/Product'
@@ -9,14 +8,14 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-       <Router>
+      <Router>
         <Header />
-        <Route path='/' exact component={Home}></Route>
-        <Route path='/about' component={About}></Route>
-        <Route path='/product' component={Product}></Route>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/about" component={About}></Route>
+        <Route path="/product" component={Product}></Route>
         <Footer />
-       </Router>
+      </Router>
     </div>
-  );
+  )
 }
-export default App;
+export default App
